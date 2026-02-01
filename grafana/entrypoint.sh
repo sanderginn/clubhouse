473b8a1a6b67fc1fe5e9be34415cdb1d30d8a0b0
Sander Ginn <sander@ginn.it>
2026-02-01 15:58:04 +0100

    chore: restore startup dir for grafana

--- grafana/entrypoint.sh
+++ grafana/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Grafana expects the bundled plugins directory to exist; create it to avoid startup warnings.
+mkdir -p /usr/share/grafana/plugins-bundled
+
 # Grafana 11+ bundles xychart; remove any stale external plugin to avoid double registration.
 if [ -d /var/lib/grafana/plugins ]; then
   for plugin_dir in /var/lib/grafana/plugins/*; do
