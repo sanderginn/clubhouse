9128cbb432360a27861135d06c2874661d0d269b
Sander Ginn <sander@ginn.it>
2026-02-10 10:04:17 +0100

    feat(issue-875): add books components barrel exports

---
+++ frontend/src/components/books/index.ts
@@ -0,0 +1,5 @@
+export { default as BookCard } from './BookCard.svelte';
+export { default as BookshelfSaveButton } from './BookshelfSaveButton.svelte';
+export { default as ReadButton } from './ReadButton.svelte';
+export { default as BookStatsBar } from './BookStatsBar.svelte';
+export { default as Bookshelf } from './Bookshelf.svelte';
